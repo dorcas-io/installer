97a78f7687187f13bbf6753d8dc31e9b22f4656b
Ifeoluwa Olawoye <bolaji.olawoye@hostvillenigeria.org>
2021-01-04 16:08:10 +0100

    updates to package.json, installation re-ordering  and re-wording

--- src/lib/inquirer.js
+++ src/lib/inquirer.js
@@ -1,108 +1,113 @@
-exports.inquiries =  [
-          {
-              name: "template",
-              type: "list",
-              message: "Please Select Dorcas Version ",
-              choices: ["business", "developer"],
-              default: "service"
-            },
-            {
-              name: "firstname",
-              type: "input",
-              message: "Please Provide Your First name",
-              when: answers => answers.template === "business" || answers.template === "developer"  ,
-              validate: function(value) {
-                if (value.length) {
-                  return true;
-                } else {
-                  return "Please enter your first name";
-                }
-              }
-            },
-            {
-              name: "lastname",
-              type: "input",
-              message: "Please Provide Your Last name",
-              when: answers => answers.template === "business" || answers.template === "developer"  ,
-              validate: function(value) {
-                if (value.length) {
-                  return true;
-                } else {
-                  return "Please enter your laest nam";
-                }
-              }
-            },
-            {
-              name: "email",
-              type: "input",
-              message: "Create Your Login Email Address",
-              validate: function(value) {
-                if (value.length) {
-                  return true;
-                } else {
-                  return "Please enter your email address";
-                }
-              }
-            },
-            
-            {
-              name: "password",
-              type: "password",
-              message: "Create Your Login Password",
-              validate: function(value) {
-                if (value.length) {
-                  return true;
-                } else {
-                  return "Please enter your password ";
-                }
-              }
-            },
-            {
-              name: "company",
-              type: "input",
-              message: "Please Provide Your company name",
-              when: answers => answers.template === "business" || answers.template === "developer"  ,
-              validate: function(value) {
-                if (value.length) {
-                  return true;
-                } else {
-                  return "Please enter your company name";
-                }
-              }
-            },
-            {
-              name: "phone",
-              type: "input",
-              message: "Please Provide Your phone  number",
-              when: answers => answers.template === "business" || answers.template === "developer"  ,
-              validate: function (value) {
-                if (value.length) {
-                  return true;
-                } else {
-                  return "Please enter a valid phone number";
-                }
-              }
-            },
-              {
-              name: "feature_select",
-              type: "list",
-              message: "Please Select The  Business Features Do You Need?",
-                  choices: [
-                      {
-                  name: "Everything!",
-                    value: "all"
-                  },
-                      {
-                  name: "Payroll",
-                    value: "payroll"
-                  },
-                      {
-                  name: "Selling Online",
-                    value: "selling_online"
-                  },
-                ],
-              default: "service"
-            },
-            
-        
-      ];
+exports.inquiries = [
+  {
+    name: "template",
+    type: "list",
+    message: "What version of the Business Edition do you wish to install? ",
+    choices: ["business", "developer"],
+    default: "business"
+  },
+  {
+    name: "firstname",
+    type: "input",
+    message: "Please enter your First Name",
+    when: answers =>
+      answers.template === "business" || answers.template === "developer",
+    validate: function(value) {
+      if (value.length) {
+        return true;
+      } else {
+        return "Required! Please enter your First Name ";
+      }
+    }
+  },
+  {
+    name: "lastname",
+    type: "input",
+    message: "Please enter your Last Name ",
+    when: answers =>
+      answers.template === "business" || answers.template === "developer",
+    validate: function(value) {
+      if (value.length) {
+        return true;
+      } else {
+        return "Required! Please enter your First Name ";
+      }
+    }
+  },
+  {
+    name: "email",
+    type: "input",
+    message:
+      "Please enter your Email Address (will be used as Admin/Login account) ",
+    validate: function(value) {
+      if (value.length) {
+        return true;
+      } else {
+        return "Required! Please enter your Email Address ";
+      }
+    }
+  },
+
+  {
+    name: "password",
+    type: "password",
+    message: "Please enter your Login Password ",
+    validate: function(value) {
+      if (value.length) {
+        return true;
+      } else {
+        return "Required! Please enter your Login Password ";
+      }
+    }
+  },
+  {
+    name: "company",
+    type: "input",
+    message: "Enter your Company or Business Name?",
+    default: "Developer",
+    when: answers => answers.template === "business",
+    validate: function(value) {
+      if (value.length) {
+        return true;
+      } else {
+        return "Required! Enter your Company or Business Name";
+      }
+    }
+  },
+  {
+    name: "phone",
+    type: "input",
+    message: "Please enter your Phone Number ",
+    when: answers =>
+      answers.template === "business" || answers.template === "developer",
+    validate: function(value) {
+      if (value.length) {
+        return true;
+      } else {
+        return "Required! Please enter your Phone Number";
+      }
+    }
+  },
+  {
+    name: "feature_select",
+    type: "list",
+    message: "Please select your needed features",
+    default: "all",
+    when: answers => answers.template === "business",
+    choices: [
+      {
+        name: "All Features",
+        value: "all"
+      },
+      {
+        name: "Payroll Features",
+        value: "payroll"
+      },
+      {
+        name: "Sales and ECommerce Features",
+        value: "selling_online"
+      }
+    ],
+    default: "all"
+  }
+];
--- src/lib/main.js
+++ src/lib/main.js
@@ -1,254 +1,237 @@
-const fs = require('fs');
-const Listr = require('listr')
-const ncp = require('ncp')
-const path = require('path')
-const envfile = require('envfile')
-const util = require('util')
-const chalk = require('chalk');
-const clear = require('clear');
-const figlet = require('figlet');
+const fs = require("fs");
+const Listr = require("listr");
+const ncp = require("ncp");
+const path = require("path");
+const envfile = require("envfile");
+const util = require("util");
+const chalk = require("chalk");
+const clear = require("clear");
+const figlet = require("figlet");
 const spawn = require("child_process").spawn;
-const CLI = require('clui');
+const CLI = require("clui");
 const Spinner = CLI.Spinner;
 const access = util.promisify(fs.access);
 const copy = util.promisify(ncp);
-const axios  = require('axios');
-const mysql  = require('mysql');
-
-
+const axios = require("axios");
+const mysql = require("mysql");
 
 clear();
 console.log(
-    chalk.yellow(
-        figlet.textSync('DORCAS-BUSINESS', { horizontalLayout: 'full' })
-    )
+  chalk.yellow(
+    figlet.textSync("Dorcas Installer", { horizontalLayout: "full" })
+  )
 );
 
+async function createProject(options) {
+  const status = new Spinner("Initializing Installation...");
+  await status.start();
 
+  options = {
+    ...options,
+    targetDirectory:
+      options.targetDirectory || process.cwd() + `/dorcas-business`
+  };
 
+  const fullPathName = __dirname + "/main.js";
+  const templateDir = path.resolve(
+    fullPathName.substr(fullPathName.indexOf("/")),
+    "../../templates",
+    options.template.toLowerCase()
+  );
+  options.templateDirectory = templateDir;
 
-//this appends env credentials for clientid and secret to env_hub
-async function writeFilesToEnv(options) {
-  let sourcePath = options.targetDirectory + `/app/env_hub`;
-  let data = {
-    DORCAS_CLIENT_ID: options.clientId,
-    DORCAS_CLIENT_SECRET: options.clientSecret,
-    DORCAS_PERSONAL_CLIENT_ID: options.clientId,
-    DORCAS_PERSONAL_CLIENT_SECRET: options.clientSecret,
+  try {
+    await installTemplateFiles(options);
+    console.log(
+      "%s " +
+        options.template.toUpperCase() +
+        " Version Template Files Installed",
+      chalk.green.bold("Success")
+    );
+    await access(templateDir, fs.constants.R_OK);
+    await status.stop();
+  } catch (err) {
+    console.log(err);
+    console.error(
+      "%s Error Installatig " +
+        options.template.toUpperCase() +
+        " Template Files",
+      chalk.red.bold("ERROR")
+    );
+    await status.stop();
+    process.exit(1);
   }
 
-  fs.appendFileSync(sourcePath, envfile.stringify(data))
+  await installContainerServices(options);
+
+  return true;
 }
 
- async function checkTable(callback){
-   const  connection =  mysql.createConnection({
-     host: "127.0.0.1",
-     user:"root",
-     password: "root",
-     port: '18008',
-     database: "dorcas"
-   });
-   const status = new Spinner('Connecting to Database...');
-   status.start();
-   connection.query("SELECT * FROM oauth_clients",   async function(err, result, fields) {
-     if (err) {
-        console.log('%s Still Initializing Tables', chalk.red.bold('error'));
-         await status.stop();
-         connection.end();
-          callback(false)
-     } else {
-       console.log('%s Connection Instantiated', chalk.green.bold('success'));
-        await status.stop();
-        connection.end();
-         callback(true)
-     }
-   });
- }
+async function installTemplateFiles(options) {
+  copy(options.templateDirectory, `${options.targetDirectory}`, {
+    clobber: false
+  });
+}
 
- async function checkConnection(callback) {
-   const  connection =  mysql.createConnection({
-     host: "127.0.0.1",
-     user:"root",
-     password: "root",
-     port: '18008',
-     database: "dorcas"
-   });
-    const status = new Spinner('Connecting to Database...');
-     status.start();
-     connection.connect(   async function(err) {
-      if (err) {
-        callback(false)
-        console.log('%s Connection Failed', chalk.red.bold('error'));
-        connection.end();
-        await status.stop()
-      }
-      else {
-        await status.stop()
-        console.log('%s Connection Established', chalk.green.bold('success'));
-        connection.end();
-        callback(true)
+async function installContainerServices(options) {
+  const status = new Spinner(
+    "Removing any existing Docker Container Services..."
+  );
+  status.start();
 
+  try {
+    const ls = await spawn("docker-compose", [
+      `-f`,
+      `${options.targetDirectory + `/docker-compose.yml`}`,
+      `down`,
+      `-v`
+    ]);
+    ls.on("close", async code => {
+      await status.stop();
+      if (code === 0) {
+        console.log(
+          "%s All Docker Container Services Removed",
+          chalk.green.bold("Success")
+        );
+        await installContainersForCore(options);
       }
     });
-
-  }
-
-async function createUserEntry(body) {
-  let res = await axios.post(`http://localhost:18001/register`, body).catch(err => {
-    console.log(chalk.red.bold(`${err}`))
-    // console.log(chalk.red.bold(`${err.response.data.errors.source}`))
-    // if(err.response.data.errors.source){
-    //   let errors = err.response.data.errors.source
-    //   if(errors.email){
-    //     console.log(`%s email`, chalk.green.bold(`${errors.email}`));
-    //   }
-    //   if(errors.phone){
-    //     console.log(`%s phone`, chalk.green.bold(`${errors.phone}`));
-    //   }
-    //   if(errors.password){
-    //     console.log(`%s password`, chalk.green.bold(`${errors.password}`));
-    //   }
-    // }
-    process.exit()
-  });
-  return res.data.data;
-}
-
-async function setUp() {
-  let res = await axios.post(`http://localhost:18001/setup`).catch(err => { console.log(chalk.red.bold(`${err}`));})
-  return res.data;
-}
- async function runBaseDockerCompose(options) {
-   const status = new Spinner('Setting Up The Dorcas Requirements...');
-   status.start();
-   try {
-    const ls = await spawn('docker-compose', [`-f`, `${options.targetDirectory + `/docker-compose.yml`}`, `up`, `-d`, `core_${options.template}_web`,  `core_${options.template}_php`,  `dorcas_${options.template}_sql`, `dorcas_${options.template}_hub_sql`,`dorcas_${options.template}_redis`, `dorcas_${options.template}_smtp`]);
-     ls.on('close', async code => {
-         await status.stop()
-         if(code === 0){
-           console.log('%s Api Setup Complete', chalk.green.bold('Success'));
-           await handleSetup(options)
-         }
-         else {
-           console.log('%s something went wrong with the Api Setup', chalk.red.bold('Error'));
-           process.exit(1);
-         }
-
-        });
-    }
-    catch(err){
-      status.stop()
-
-    }
-    finally {
+  } catch (err) {
+    await status.stop();
+  } finally {
   }
 }
 
-async function copyTemplateFiles(options) {
-  copy(options.templateDirectory, `${options.targetDirectory}`, {
-    clobber: false,
-  });
-}
-
-async function runHubDockerCompose(options){
-    const status = new Spinner('Setting Up The Hub...');
-    status.start();
-    try{
-    const ls = await spawn('docker-compose', [`-f`, `${options.targetDirectory +`/docker-compose.yml`}`,`up`, `-d`, `hub_${options.template}_web`,`hub_${options.template}_php`]);
-      ls.on('close', async code => {
-        await status.stop();
-        if(code === 0){
-          console.log('%s Hub Setup Complete', chalk.green.bold('Success'));
-          await finalUerSetup(options)
-        }
-
-      });
-    }
-    catch(err){
-      await status.stop();
-    }
-    finally {
-
-    }
-
-
-}
-
-async function resetServices(options){
-    const status = new Spinner('Pulling Down Services...');
-    status.start();
-    try{
-    const ls = await spawn('docker-compose', [`-f`, `${options.targetDirectory +`/docker-compose.yml`}`,`down`, `-v`]);
-      ls.on('close', async code => {
-        await status.stop();
-        if(code === 0){
-          console.log('%s All  Services Removed', chalk.green.bold('Success'));
-          await runBaseDockerCompose(options)
-
-        }
-
-      })
-    }
-    catch(err){
-      await status.stop();
-    }
-    finally {
-
-    }
-
-
-}
-
-async function handleSetup(options) {
-  const status = new Spinner('Setting Up Enviroment Variables...');
-  const tasks = new Listr([{ title: 'Setting Up Dorcas Hub', task: () => runHubDockerCompose(options), },], { exitOnError: false, });
+async function installContainersForCore(options) {
+  const status = new Spinner("Installing Containers for Dorcas CORE");
   status.start();
-   try {
-     await checkConnection(async function(result) {
-      if (result){
-        await checkTable(async function(result) {
-          if(result){
+  try {
+    const ls = await spawn("docker-compose", [
+      `-f`,
+      `${options.targetDirectory + `/docker-compose.yml`}`,
+      `up`,
+      `-d`,
+      `core_${options.template}_web`,
+      `core_${options.template}_php`,
+      `dorcas_${options.template}_sql`,
+      `dorcas_${options.template}_hub_sql`,
+      `dorcas_${options.template}_redis`,
+      `dorcas_${options.template}_smtp`
+    ]);
+    ls.on("close", async code => {
+      await status.stop();
+      if (code === 0) {
+        console.log(
+          "%s Dorcas CORE Installation Complete",
+          chalk.green.bold("Success")
+        );
+        await prepareContainersForHub(options);
+      } else {
+        console.log(
+          "%s Dorcas CORE Installation Error!",
+          chalk.red.bold("Error")
+        );
+        process.exit(1);
+      }
+    });
+  } catch (err) {
+    status.stop();
+  } finally {
+  }
+}
+
+async function prepareContainersForHub(options) {
+  const status = new Spinner("Preparing Dorcas HUB Installation...");
+
+  const tasks = new Listr(
+    [
+      {
+        title: "Setting Up Dorcas HUB",
+        task: () => installContainersForHub(options)
+      }
+    ],
+    { exitOnError: false }
+  );
+
+  status.start();
+
+  try {
+    await checkDatabaseConnection(async function(result) {
+      if (result) {
+        await checkDatabaseTables(async function(result) {
+          if (result) {
             setTimeout(async () => {
-              let res = await setUp(options)
-              options.clientId = res.client_id
-              options.clientSecret = res.client_secret
-              if (typeof options.clientId !== 'undefined') {
-                await writeFilesToEnv(options)
+              let res = await setupDorcasCoreOAuth(options);
+              options.clientId = res.client_id;
+              options.clientSecret = res.client_secret;
+              if (typeof options.clientId !== "undefined") {
+                console.log(
+                  "%s Dorcas HUB OAuth Set",
+                  chalk.green.bold("Success")
+                );
+                await setupDorcasHubENV(options);
                 await status.stop();
-                console.log('%s Enviroment Variables All Set ', chalk.green.bold('Success'));
+                console.log(
+                  "%s Dorcas HUB ENV Set",
+                  chalk.green.bold("Success")
+                );
                 await tasks.run();
               }
-            }, 3000)
-          }
-          else{
+            }, 3000);
+          } else {
             setTimeout(async () => {
-              console.log('creating required tables');
-              await status.stop()
+              console.log("Creating Database tables");
+              await status.stop();
               await handleSetup(options);
-            },3000)
+            }, 3000);
           }
-        })
-      }
-      else{
-         setTimeout(async () => {
-         console.log('retrying the connection');
+        });
+      } else {
+        setTimeout(async () => {
+          console.log("Retrying Database connection...");
           await status.stop();
           await handleSetup(options);
-        },3000)
+        }, 3000);
       }
-     });
+    });
+  } catch (e) {
+    await status.stop();
+  } finally {
+  }
+}
 
-   }
-   catch (e) {
-     await status.stop();
-   }
-   finally {
+async function installContainersForHub(options) {
+  const status = new Spinner("Installing Containers for Dorcas HUB");
+  status.start();
 
-   }
+  try {
+    const ls = await spawn("docker-compose", [
+      `-f`,
+      `${options.targetDirectory + `/docker-compose.yml`}`,
+      `up`,
+      `-d`,
+      `hub_${options.template}_web`,
+      `hub_${options.template}_php`
+    ]);
+    ls.on("close", async code => {
+      await status.stop();
+      if (code === 0) {
+        console.log(
+          "%s Dorcas HUB Installation Complete",
+          chalk.green.bold("Success")
+        );
+        await finalUerSetup(options);
+      }
+    });
+  } catch (err) {
+    console.log("%s Dorcas HUB Installation Error!", chalk.red.bold("Error"));
+    await status.stop();
+  } finally {
+  }
 }
 
 async function finalUerSetup(options) {
-  const status = new Spinner('Setting Up User Credentials...');
+  const status = new Spinner("Setting Up User Credentials...");
   status.start();
   try {
     setTimeout(async () => {
@@ -262,76 +245,109 @@ async function finalUerSetup(options) {
         phone: options.answers.phone,
         feature_select: options.answers.feature_select,
         client_id: options.clientId,
-        client_secret: options.clientSecret,
+        client_secret: options.clientSecret
+      };
+      let res = await createUserEntry(data);
+      if (typeof res !== "undefined") {
+        await status.stop();
+        console.log("%s You are all Set", chalk.green.bold("Success"));
+        console.log("\n");
+        console.log(
+          "Thank You for Signing Up " +
+            res.email +
+            " Kindly Visit " +
+            chalk.green.bold("http://localhost:8001/login") +
+            " To Access The Hub"
+        );
       }
-       let res = await createUserEntry(data)
-        if (typeof res !== 'undefined') {
-          await status.stop();
-          console.log('%s You are all Set', chalk.green.bold('Success'));
-          console.log('\n');
-          console.log('Thank You for Signing Up ' + res.email + ' Kindly Visit '+ chalk.green.bold('http://localhost:8001/login') + ' To Access The Hub');
-       }
-     }, 2000)
-    
-  }
-  catch (err) {
+    }, 2000);
+  } catch (err) {
     await status.stop();
   }
 }
 
+async function createUserEntry(body) {
+  let res = await axios
+    .post(`http://localhost:18001/register`, body)
+    .catch(err => {
+      console.log(chalk.red.bold(`${err}`));
+      process.exit();
+    });
+  return res.data.data;
+}
 
-async function createProject(options) {
-  // checkConnection(async function(result) {
-  //   if (result){
-  //     await checkTable(async function(result) {
-  //       if(result){
-  //         setTimeout(async () => {
-  //           console.log('we good')
-  //         }, 3000)
-  //       }
-  //       else{
-  //         setTimeout(async () => {
-  //           console.log('creating required tables\n');
-  //           await createProject(options);
-  //         },3000)
-  //       }
-  //     })
-  //   }
-  //   else{
-  //     setTimeout(async () => {
-  //       console.log('retrying the connection\n');
-  //       await createProject(options);
-  //     },3000)
-  //   }
-  // });
-   const status = new Spinner('Initializing...');
-   await status.start();
-    options = {
-    ...options,
-    targetDirectory: options.targetDirectory || process.cwd()+`/dorcas`,
-    };
-  // const fullPathName = new URL(import.meta.url).pathname;
-  const fullPathName = __dirname + '/main.js';
-  const templateDir = path.resolve(
-    fullPathName.substr(fullPathName.indexOf('/')),
-    '../../templates',
-    options.template.toLowerCase()
-  );
-   options.templateDirectory = templateDir;
-  try {
-    await copyTemplateFiles(options)
-    console.log('%s Template Files Created', chalk.green.bold('Success'));
-    await access(templateDir, fs.constants.R_OK);
-    await status.stop();
+//this appends env credentials for clientid and secret to env_hub
+async function setupDorcasHubENV(options) {
+  let sourcePath = options.targetDirectory + `/app/env_hub`;
+  let data = {
+    DORCAS_CLIENT_ID: options.clientId,
+    DORCAS_CLIENT_SECRET: options.clientSecret,
+    DORCAS_PERSONAL_CLIENT_ID: options.clientId,
+    DORCAS_PERSONAL_CLIENT_SECRET: options.clientSecret
+  };
 
-  } catch (err) {
-    console.log(err)
-    console.error('%s Invalid template name', chalk.red.bold('ERROR'));
-    await status.stop()
-    process.exit(1);
-  }
-  await resetServices(options)
-  return true;
- }
+  fs.appendFileSync(sourcePath, envfile.stringify(data));
+}
+
+async function checkDatabaseConnection(callback) {
+  const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    port: "18008",
+    database: "dorcas"
+  });
+  const status = new Spinner("Connecting to Database...");
+  status.start();
+  connection.connect(async function(err) {
+    if (err) {
+      callback(false);
+      console.log("%s Connection Failed", chalk.red.bold("error"));
+      connection.end();
+      await status.stop();
+    } else {
+      await status.stop();
+      console.log("%s Connection Established", chalk.green.bold("success"));
+      connection.end();
+      callback(true);
+    }
+  });
+}
+
+async function checkDatabaseTables(callback) {
+  const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "root",
+    port: "18008",
+    database: "dorcas"
+  });
+  const status = new Spinner("Connecting to Database...");
+  status.start();
+  connection.query("SELECT * FROM oauth_clients", async function(
+    err,
+    result,
+    fields
+  ) {
+    if (err) {
+      console.log("%s Still Initializing Tables", chalk.red.bold("error"));
+      await status.stop();
+      connection.end();
+      callback(false);
+    } else {
+      console.log("%s Connection Instantiated", chalk.green.bold("success"));
+      await status.stop();
+      connection.end();
+      callback(true);
+    }
+  });
+}
+
+async function setupDorcasCoreOAuth() {
+  let res = await axios.post(`http://localhost:18001/setup`).catch(err => {
+    console.log(chalk.red.bold(`${err}`));
+  });
+  return res.data;
+}
 
 exports.createProject = createProject;
